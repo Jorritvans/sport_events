06625c1068086ba43ddf9e59ad887e0ac78a46e4
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-08-02 14:22:01 +0000

    adding comments in the models.py file for clarity

--- events/models.py
+++ events/models.py
@@ -1,19 +1,37 @@
 from django.db import models
 
+# Model representing an event
 class Event(models.Model):
+    # Name of the event
     name = models.CharField(max_length=100)
+
+    # Detailed description of the event
     description = models.TextField()
+
+    # Date and time when the event will occur
     date = models.DateTimeField()
+
+    # Location where the event will be held
     location = models.CharField(max_length=100)
+
+    # Image associated with the event
     image = models.ImageField(upload_to='event_images/')
 
     def __str__(self):
         return self.name
 
+# Model representing a booking for an event
 class Booking(models.Model):
+    # Event associated with the booking
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    # Name of the person making the booking
     name = models.CharField(max_length=100)
+
+    # Email of the person making the booking
     email = models.EmailField()
+
+    # Number of tickets booked
     number_of_tickets = models.PositiveIntegerField()
 
     def __str__(self):
