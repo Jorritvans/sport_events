a13bb04336214a6cbd1a8e1c51bc1ec27fc7d927
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-08-02 12:00:32 +0000

    define models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='event_images/')
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    number_of_tickets = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.event.name}"
