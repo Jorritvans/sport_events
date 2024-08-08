1a85c5638f6d21ad79d26d130fe63250bc2d46e2
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-08-08 15:35:28 +0000

    removing contacts from django admin

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Event, Booking, ContactSubmission
+from .models import Event, Booking
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'location', 'price')
@@ -9,10 +9,5 @@ class BookingAdmin(admin.ModelAdmin):
     list_display = ('event', 'user', 'email', 'date_booked', 'number_of_tickets')
     search_fields = ('event__title', 'user__username')
 
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'message', 'submitted_at')
-    search_fields = ('name', 'email', 'message')
-
 admin.site.register(Event, EventAdmin)
 admin.site.register(Booking, BookingAdmin)
-admin.site.register(ContactSubmission)
