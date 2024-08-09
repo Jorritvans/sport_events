f8d94d765030f3e8065cb220da25ce42dfaa1eef
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-08-09 06:57:45 +0000

    fixed database after complete reset.

---
+++ events/management/commands/clear_migrations.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.db import connection
+
+class Command(BaseCommand):
+    help = 'Clears migration history for the events app'
+
+    def handle(self, *args, **kwargs):
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM django_migrations WHERE app = 'events';")
+        self.stdout.write(self.style.SUCCESS('Successfully cleared migration history for the events app'))
