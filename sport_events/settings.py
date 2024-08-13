c180e9f8afbd515efee558817ececf105e5d355b
Jorrit Vanstraelen <jorritvanstraelen11@gmail.com>
2024-08-13 17:42:03 +0000

    changing database info

--- sport_events/settings.py
+++ sport_events/settings.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 import dj_database_url
 from decouple import config
 import logging.config
+from pathlib import Path
 
 if os.path.isfile('env.py'):
     import env
 # Load environment variables from .env file
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -17,7 +18,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY',
 )
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-jorritvans-sportevents-mxbvc73wfzr.ws-eu115.gitpod.io',
@@ -68,7 +69,7 @@ WSGI_APPLICATION = 'sport_events.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': config('DATABASE_NAME', default='db.sqlite3'),
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
